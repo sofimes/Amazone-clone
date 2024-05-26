b0b76f297eefd4988d393920072f03e0b9f41e20
sofimes <sofanitmesfin19@gmail.com>
2024-05-26 15:49:39 +0300

    backend start by function from firebase

---
+++ amazon-clone/functions/index.js
@@ -0,0 +1,30 @@
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to stripe payment gateway",
+  });
+});
+app.post("/payment/create", async (req, res) => {
+  const total = req.query.total;
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(403).json({ message: "Total must be greater than zero" });
+  }
+});
+exports.api = onRequest(app);
