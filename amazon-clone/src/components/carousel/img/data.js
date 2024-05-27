cf1b5d4064a09db9a40d990b7c06ca44f47cc566
sofimes <sofanitmesfin19@gmail.com>
2024-05-27 16:43:15 +0300

    some image changes for the anner

--- amazon-clone/src/components/carousel/img/data.js
+++ amazon-clone/src/components/carousel/img/data.js
@@ -1,8 +1,8 @@
-import img11 from "./img11.jpeg";
-import img12 from "./img12.jpeg";
-import img13 from "./img13.jpeg";
-import img14 from "./img14.png";
-import img15 from "./img15.jpeg";
-import img6 from "./img6.jpeg";
+import img11 from "./img11.jpg";
+import img12 from "./img12.jpg";
+import img13 from "./img13.jpg";
+import img14 from "./img14.jpg";
+import img15 from "./img15.jpg";
+import img6 from "./img6.jpg";
 
 export const img = [img11, img12, img13, img14, img15, img6];
